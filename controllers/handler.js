cfc32225ddd2d5e6d2116b0ce002f73b03dbbe16
Guillaume Des Rosiers <guillaumedesrosiers@Guillaumes-MacBook-Air.local>
2022-08-09 12:41:23 -0400

    api and handler update

--- controllers/handler.js
+++ controllers/handler.js
@@ -1,23 +1,37 @@
-// const mongoose = require('mongoose');
-// const mongo_URI = process.env.mongo_URI;
-// mongoose.connect(mongo_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+const mongoose = require('mongoose');
+const mongo_URI = process.env.MONGO_URI;
+mongoose.connect(mongo_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// const stockSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   price: { type: Number, required: true },
-//   likes: { type: Number, required: true },
-// });
+const stockSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  price: {type: Number, required: true},
+  likes: {type: Number, required: true},
+});
 
-// const Stock = mongoose.model('Stock', stockSchema);
+const Stock = mongoose.model('Stock', stockSchema);
 
 function Handler() {
-  this.findOrCreateStock = (stockSymbol, done) => {
-    Stock.findOne({ name: stockSymbol }, (err, stock) => {
-      if (err) return done(err);
-      if (stock === null) {
+  this.findOneAndUpdateOrCreate = (stockSymbol, price, likes, done) => {
+    Stock.findOne({name: stockSymbol}, (err, stock) => {
+      if (err) return done(err, null);
+      if (stock) {
+        stock.price = price;
+        stock.likes += likes;
+        stock.save().then((savedStock) => {
+          return done(null, savedStock);
+        });
+      } else {
+        let newStock = new Stock({
+          name: stockSymbol,
+          price: price,
+          likes: likes,
+        });
+        newStock.save().then((savedStock) => {
+          return done(null, savedStock);
+        });
       }
     });
   };
--- routes/api.js
+++ routes/api.js
@@ -3,50 +3,52 @@ const Handler = require('../controllers/handler.js');
 const https = require('https');
 const axios = require('axios');
 
-const mongoose = require('mongoose');
-const { contentSecurityPolicy } = require('helmet');
-const { setUncaughtExceptionCaptureCallback } = require('process');
-const mongo_URI = process.env.mongo_URI;
-mongoose.connect(mongo_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// const mongoose = require('mongoose');
+const {contentSecurityPolicy} = require('helmet');
+const {setUncaughtExceptionCaptureCallback, nextTick} = require('process');
+const {privateDecrypt} = require('crypto');
+// const mongo_URI = process.env.MONGO_URI;
+// mongoose.connect(mongo_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-const stockSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  likes: { type: Number, required: true },
-});
+// const stockSchema = new mongoose.Schema({
+//   name: { type: String, required: true },
+//   price: { type: Number, required: true },
+//   likes: { type: Number, required: true },
+// });
 
-const Stock = mongoose.model('Stock', stockSchema);
+// const Stock = mongoose.model('Stock', stockSchema);
 
 module.exports = function (app) {
-  let handler = new Handler();
   app.route('/api/stock-prices').get(function (req, res, next) {
-    let stockSymbol = req.query.stock;
+    let handler = new Handler();
     let likes = 0;
-    if (req.query.like === true) {
-      likes = 1;
-    }
-
-    axios
-      .get(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stockSymbol}/quote`)
-      .then((response) => {
-        Stock.findOne({ name: stockSymbol }, (err, stock) => {
-          if (err) return next(err);
-          if (stock) {
+    if (req.query.like === 'true') likes = 1;
+    if (!Array.isArray(req.query.stock)) {
+      let stockSymbol = req.query.stock;
+      axios
+        .get(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stockSymbol}/quote`)
+        .then((response) => {
+          handler.findOneAndUpdateOrCreate(stockSymbol, response.data['latestPrice'], likes, (err, stock) => {
+            if (err) return next(err);
             res.json({
               name: stock.name,
               price: stock.price,
               likes: stock.likes,
             });
-          } else {
-            const newStock = new Stock({
-              name: stockSymbol,
-              price: response.data['latestPrice'],
-              likes: likes,
-            });
-            newStock.save((err, stock) => {
+          });
+        })
+        .catch((err) => {
+          return next(err);
+        });
+    } else {
+      req.query.stock.forEach((stockSymbol) => {
+        axios
+          .get(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stockSymbol}/quote`)
+          .then((response) => {
+            handler.findOneAndUpdateOrCreate(stockSymbol, response.data['latestPrice'], likes, (err, stock) => {
               if (err) return next(err);
               res.json({
                 name: stock.name,
@@ -54,43 +56,11 @@ module.exports = function (app) {
                 likes: stock.likes,
               });
             });
-          }
-        });
-      })
-      .catch((error) => {
-        console.error(error);
+          })
+          .catch((err) => {
+            return next(err);
+          });
       });
-    // findOrCreate Stock Schema
+    }
   });
 };
-
-// https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/[symbol]/quote
-// symbol = msft | goog | aapl | ...
-
-// https.get(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stockSymbol}/quote`, (response) => {
-//   let data = '';
-//   response.on('data', (chunk) => {
-//     data += chunk;
-//   });
-//   response
-//     .on('end', () => {
-//       let dataJSON = JSON.parse(data);
-//       price = dataJSON.latestPrice;
-//       // console.log(price);
-//     })
-//     .on('error', (error) => {
-//       return res.send(error);
-//     });
-// });
-
-// Stock.findOne({ name: stockSymbol }, (err, stock) => {
-//   if (err) return next(err);
-//   if (stock === null) {
-//     Stock.create({ name: stockSymbol, price: price, likes: 0 }, (err, stock) => {
-//       if (err) return next(err);
-//       console.log(stock);
-//       return res.json(stock);
-//     });
-//   } else {
-//   }
-// });
