78bb12a2dd5a9c1115a141277ec19dda044a3c8d
mesieu <guizmo.drs@gmail.com>
2022-08-12 15:37:57 -0400

    fragmented handler functions for better architecture

--- controllers/handler.js
+++ controllers/handler.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const mongoose = require('mongoose');
 const mongo_URI = process.env.MONGO_URI;
 mongoose.connect(mongo_URI, {
@@ -14,25 +16,87 @@ const stockSchema = new mongoose.Schema({
 const Stock = mongoose.model('Stock', stockSchema);
 
 function Handler() {
-  this.findOneAndUpdateOrCreate = (stockSymbol, price, likes, done) => {
+  this.getStockPrice = (stockSymbol, done) => {
+    axios
+      .get(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stockSymbol}/quote`)
+      .then((response) => {
+        return done(null, reponse.data['latestPrice']);
+      })
+      .catch((err) => {
+        return done(err, null);
+      });
+  };
+
+  this.findStock = (stockSymbol, done) => {
     Stock.findOne({name: stockSymbol}, (err, stock) => {
       if (err) return done(err, null);
-      if (stock) {
-        stock.price = price;
-        stock.likes += likes;
-        stock.save().then((savedStock) => {
-          return done(null, savedStock);
-        });
-      } else {
-        let newStock = new Stock({
-          name: stockSymbol,
-          price: price,
-          likes: likes,
-        });
-        newStock.save().then((savedStock) => {
-          return done(null, savedStock);
-        });
-      }
+      return done(null, stock);
+    });
+  };
+
+  this.createStock = (stockSymbol, price, likes, done) => {
+    let newStock = new Stock({
+      name: stockSymbol,
+      price: price,
+      likes: likes,
+    });
+    newStock.save().then((savedStock) => {
+      return done(null, savedStock);
+    });
+  };
+
+  this.updateStock = (stock, price, likes, done) => {
+    stock.price = price;
+    stock.likes += likes;
+    stock.save().then((savedStock) => {
+      return done(null, savedStock);
+    });
+  };
+
+  // this.getStock = (stockSymbol, likes, done) => {
+  //   axios
+  //     .get(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stockSymbol}/quote`)
+  //     .then((response) => {
+  //       this.findOneAndUpdateOrCreate(stockSymbol, response.data['latestPrice'], likes, (err, stock) => {
+  //         if (err) return done(err, null);
+  //         return done(null, {
+  //           name: stock.name,
+  //           price: stock.price,
+  //           likes: stock.likes,
+  //         });
+  //       });
+  //     })
+  //     .catch((err) => {
+  //       return done(err, null);
+  //     });
+  // };
+
+  // this.findOneAndUpdateOrCreate = (stockSymbol, price, likes, done) => {
+  //   Stock.findOne({name: stockSymbol}, (err, stock) => {
+  //     if (err) return done(err, null);
+  //     if (stock) {
+  //       stock.price = price;
+  //       stock.likes += likes;
+  //       stock.save().then((savedStock) => {
+  //         return done(null, savedStock);
+  //       });
+  //     } else {
+  //       let newStock = new Stock({
+  //         name: stockSymbol,
+  //         price: price,
+  //         likes: likes,
+  //       });
+  //       newStock.save().then((savedStock) => {
+  //         return done(null, savedStock);
+  //       });
+  //     }
+  //   });
+  // };
+
+  this.getStockDataArray = (firstStockSymbol, secondStockSymbol, done) => {
+    Stock.find({name: {$in: [firstStockSymbol, secondStockSymbol]}}, (err, stocks) => {
+      if (err) return done(err, null);
+      return done(null, stocks);
     });
   };
 }
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,5 @@
 'use strict';
 const Handler = require('../controllers/handler.js');
-const axios = require('axios');
 const {contentSecurityPolicy} = require('helmet');
 const {setUncaughtExceptionCaptureCallback, nextTick} = require('process');
 const {privateDecrypt} = require('crypto');
@@ -11,57 +10,16 @@ module.exports = function (app) {
     let likes = 0;
     let stockSymbol = req.query.stock;
     if (req.query.like === 'true') likes = 1;
-    if (!Array.isArray(req.query.stock)) {
-      axios
-        .get(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stockSymbol}/quote`)
-        .then((response) => {
-          handler.findOneAndUpdateOrCreate(stockSymbol, response.data['latestPrice'], likes, (err, stock) => {
-            if (err) res.send(next(err));
-            res.json({
-              name: stock.name,
-              price: stock.price,
-              likes: stock.likes,
-            });
+    if (!Array.isArray(stockSymbol)) {
+      handler.findStock(stockSymbol, (err, stock) => {
+        if (err) return next(err);
+        if (stock) {
+          handler.getStockPrice(stockSymbol, (err, price) => {
+            if (err) return next(err);
           });
-        })
-        .catch((err) => {
-          res.send(next(err));
-        });
+        }
+      });
     } else {
-      let stockData = [];
-      let firstStockSymbol = stockSymbol[0];
-      let secondStockSymbol = stockSymbol[1];
-      axios
-        .get(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${firstStockSymbol}/quote`)
-        .then((response) => {
-          handler.findOneAndUpdateOrCreate(stockSymbol, response.data['latestPrice'], likes, (err, stock) => {
-            if (err) res.send(next(err));
-            stockData.push({
-              name: stock.name,
-              price: stock.price,
-              likes: stock.likes,
-            });
-          });
-        })
-        .catch((err) => {
-          res.send(next(err));
-        });
-      axios
-        .get(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${secondStockSymbol}/quote`)
-        .then((response) => {
-          handler.findOneAndUpdateOrCreate(stockSymbol, response.data['latestPrice'], likes, (err, stock) => {
-            if (err) res.send(next(err));
-            stockData.push({
-              name: stock.name,
-              price: stock.price,
-              likes: stock.likes,
-            });
-          });
-        })
-        .catch((err) => {
-          res.send(next(err));
-        });
-      console.log("I'm here");
     }
   });
 };
